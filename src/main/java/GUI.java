5ae6541c322185ad2934ee8a22fca6b489b2f57a
SquarePear <16364318+SquarePear@users.noreply.github.com>
2023-09-05 16:32:46 -0400

    Create initial GUI window

---
+++ src/main/java/GUI.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        configureWindow(primaryStage);
+    }
+
+    private void configureWindow(Stage stage) {
+        Scene scene = new Scene(new VBox(), 400, 600);
+
+        stage.setTitle("Cardinal Trivia - Board");
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+}
