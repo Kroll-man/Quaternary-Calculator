d29e257508e601d6ef13e484fc375bf95b9eb285
Kroll_man <wtwithers2020@gmail.com>
2023-09-05 17:42:19 -0400

    Implement toQuaternary method

--- src/main/java/QuaternaryConverter.java
+++ src/main/java/QuaternaryConverter.java
@@ -4,6 +4,15 @@ public class QuaternaryConverter {
     }
 
     public int toQuaternary(int decimal) {
-        return 0;
+        int index = 0;
+        int quaternary = 0;
+        int nextDigit;
+        while (decimal !=0) {
+            nextDigit = decimal % 4;
+            quaternary+= nextDigit * Math.pow(10,index);
+            index +=1;
+            decimal = decimal/4;
+        }
+        return quaternary;
     }
 }
--- src/test/java/QuaternaryConverterTest.java
+++ src/test/java/QuaternaryConverterTest.java
@@ -2,15 +2,42 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class QuaternaryConverterTest {
-    @Test
-    public void testToQuaternary0is0() {
-        QuaternaryConverter converter = new QuaternaryConverter();
-        Assertions.assertEquals(0, converter.toQuaternary(0));
-    }
 
     @Test
-    public void testFromQuaternary0is0() {
+    public void testQuaternary0is0() {
         QuaternaryConverter converter = new QuaternaryConverter();
         Assertions.assertEquals(0, converter.fromQuaternary(0));
     }
+
+    @Test
+    public void testToQuaternarySingeDigitIsSame() {
+        QuaternaryConverter converter = new QuaternaryConverter();
+        Assertions.assertEquals(0, converter.toQuaternary(0));
+        Assertions.assertEquals(1, converter.toQuaternary(1));
+        Assertions.assertEquals(2, converter.toQuaternary(2));
+        Assertions.assertEquals(3, converter.toQuaternary(3));
+    }
+
+    @Test
+    public void testToQuaternaryFourIsTen(){
+        QuaternaryConverter converter = new QuaternaryConverter();
+        Assertions.assertEquals(10, converter.toQuaternary(4));
+    }
+
+    @Test
+    public void testToQuaternaryLargeInput(){
+        QuaternaryConverter converter = new QuaternaryConverter();
+        Assertions.assertEquals(1110121023, converter.toQuaternary(345675));
+    }
+
+    @Test
+    public void testToQuaternaryNegative(){
+        QuaternaryConverter converter = new QuaternaryConverter();
+        Assertions.assertEquals(-10, converter.toQuaternary(-4));
+        Assertions.assertEquals(-1, converter.toQuaternary(-1));
+        Assertions.assertEquals(-2, converter.toQuaternary(-2));
+        Assertions.assertEquals(-3, converter.toQuaternary(-3));
+        Assertions.assertEquals(-1110121023, converter.toQuaternary(-345675));
+    }
+
 }
